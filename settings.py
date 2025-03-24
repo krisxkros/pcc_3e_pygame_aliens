dead25ad021490a39034883d60401405234d5cf1
krisxkros <kris836@gmail.com>
2025-03-24 08:41:25 +0100

    Added and integrated Ship class.

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ class Settings:
 
     def __init__(self):
         """Initialize the game's settings."""
+
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
