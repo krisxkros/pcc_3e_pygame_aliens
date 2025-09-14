f7e77a5cf548884d3d7db4bb9c72d2cbc73e86cd
krisxkros <kris836@gmail.com>
2025-09-14 22:06:33 +0200

    Implement ship movement  to the right and event handling in Alien Invasion game

--- alien_invasion.py
+++ alien_invasion.py
@@ -19,18 +19,32 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch the keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
             self.clock.tick(60)
+
+    def _check_events(self):
+        """Respond to key-presses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right
+                    # self.ship.rect.x += 1
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
 if __name__ == "__main__":
     # Make a game instance and run the game.
